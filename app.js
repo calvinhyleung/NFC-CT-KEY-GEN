5b920a426111d9a3cbf97bfadbad0ec4e8d919fc
Calvin Leung <calvinhyleung@outlook.com>
2020-07-01 19:30:24 +0800

    cleanup

--- app.js
+++ app.js
@@ -26,42 +26,4 @@ app.get("/", (req, res) => {
     return res.sendFile(path.join(__dirname + "/index.html"));
   });
 
-app.post('/handle', function(request, response){
-    const token = request.body.idtoken;
-    //console.log(token);
-    response.send("custom response");
-
-    const client = new OAuth2Client(clientID);
-    async function verify() {
-    const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: clientID,  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-    });
-    const payload = ticket.getPayload();
-    const aud = payload['aud'];
-    const iss = payload['iss'];
-    const full_name = payload['name'];
-    const family_name = payload['family_name'];
-    const given_name = payload['given_name'];
-    const picture = payload['picture'];
-    const email = payload['email'];
-    const email_verified = payload['email_verified'];
-    const userid = payload['sub'];
-    // If request specified a G Suite domain:
-    // const domain = payload['hd'];
-    //console.log('payload: ' + payload);
-    console.log('aud: ' + aud);
-    console.log('iss: ' + iss);
-    console.log('Full Name: ' + full_name);
-    console.log('Family Name: ' + family_name);
-    console.log('Given Name: ' + given_name);
-    console.log('Picture Link: ' + picture);
-    console.log('email: ' + email);
-    console.log('verified: ' + email_verified);
-    console.log('userid: ' + userid);
-    }
-    verify().catch(console.error);
-})
 
